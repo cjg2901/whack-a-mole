18e13600a8ee2372d5c476d65978c6003350f76e
srikamal <srikamal2001@gmail.com>
2019-04-12 13:44:27 -0400

    Mk 4.1 - created board and GUI classes

--- src/client/gui/WAMGUI.java
+++ src/client/gui/WAMGUI.java
@@ -1,10 +1,29 @@
 package client.gui;
 
+import javafx.application.Application;
+import javafx.stage.Stage;
+
 /**
  * A JavaFX GUI for the networked Whack-A-Mole Game
  * Author : Sri Kamal V. Chillarage
  */
-public class WAMGUI
+public class WAMGUI extends Application implements Observer<WAMBoard>
 {
 
+    @Override
+    public void update(WAMBoard wamBoard)
+    {
+
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception
+    {
+
+    }
+
+    public static void main(String[] args)
+    {
+
+    }
 }
