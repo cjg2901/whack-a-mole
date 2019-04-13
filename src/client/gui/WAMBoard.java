68757117c3390ef586e5a90ef392dcabe9d1ee69
srikamal <srikamal2001@gmail.com>
2019-04-13 13:28:54 -0400

    Mk 4.1 - created board and GUI classes

--- src/client/gui/WAMBoard.java
+++ src/client/gui/WAMBoard.java
@@ -1,6 +1,31 @@
 package client.gui;
 
+import java.util.List;
+
 public class WAMBoard
 {
+    /**
+     * the observers of this model
+     */
 
+    private List<Observer<WAMBoard>> observers;
+
+    /**
+     * The view calls this method to add themselves as an observer of the model
+     */
+    public void addObserver(Observer<WAMBoard> observer)
+    {
+        this.observers.add(observer);
+    }
+
+    /**
+     * When the model changes, the observers are notified via their update methid
+     */
+    private void alertObservers()
+    {
+        for(Observer<WAMBoard> obs: this.observers)
+        {
+            obs.update(this);
+        }
+    }
 }
