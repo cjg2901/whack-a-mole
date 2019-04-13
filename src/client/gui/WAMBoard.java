157241bdfed914514c5719e98a02fc3300c96d70
srikamal <srikamal2001@gmail.com>
2019-04-13 14:57:00 -0400

    Mk5 - WAMBoard partially complete

--- src/client/gui/WAMBoard.java
+++ src/client/gui/WAMBoard.java
@@ -2,13 +2,64 @@ package client.gui;
 
 import java.util.List;
 
+/**
+ * The model for the Whack-A-Mole game
+ *
+ * @author srikamal
+ */
 public class WAMBoard
 {
     /**
+     * useful variables
+     */
+    public int ROWS;
+    public int COLS;
+    public int Players;
+    public String Score;
+
+    /**
      * the observers of this model
      */
-
     private List<Observer<WAMBoard>> observers;
+
+    /**
+     * current game status
+     */
+    private Status status;
+
+    /**
+     * constructor ...(takes in number of rows and columns and number of players)
+     */
+
+    public WAMBoard(int rows, int cols, int players)
+    {
+        this.ROWS = rows;
+        this.COLS = cols;
+        this.Players = players;
+        this.status = Status.NOT_OVER;
+
+    }
+
+    /**
+     * possible status of the game
+     */
+    public enum Status
+    {
+        NOT_OVER, I_WON, I_LOST, TIE, ERROR;
+
+        private String message = null;
+
+        public void setMessage(String msg)
+        {
+            this.message = msg;
+        }
+
+        @Override
+        public String toString()
+        {
+            return super.toString() + this.message == null ? "" : ( '(' + this.message + ')' );
+        }
+    }
 
     /**
      * The view calls this method to add themselves as an observer of the model
@@ -27,5 +78,87 @@ public class WAMBoard
         {
             obs.update(this);
         }
+    }
+
+    /**
+     * get game status
+     */
+    public Status getStatus()
+    {
+        return this.status;
+    }
+
+    /**
+     * The user may close at any time.
+     */
+    public void close()
+    {
+        alertObservers();
+    }
+
+    /**
+     * Called when the game has been won by this player.
+     */
+    public void gameWon()
+    {
+        this.status = Status.I_WON;
+        alertObservers();
+    }
+
+    /**
+     * Called when the game has been won by the other player.
+     */
+    public void gameLost()
+    {
+        this.status = Status.I_LOST;
+        alertObservers();
+    }
+
+    /**
+     * Called when the game has been tied.
+     */
+    public void gameTied()
+    {
+        this.status = Status.TIE;
+        alertObservers();
+    }
+
+    /**
+     * to inform them that a mole has gone down
+     */
+    public void moleUp()
+    {
+        alertObservers();
+    }
+
+    /**
+     * to inform players that a mole has gone down
+     */
+    public void moleDown()
+    {
+        alertObservers();
+    }
+
+    /**
+     * to let the players send a whack message
+     */
+    public void Wack(int col, int row)
+    {
+        //hit at location board[row][col]
+    }
+
+    public void Error(String arguments)
+    {
+        this.status = Status.ERROR;
+        this.status.setMessage(arguments);
+        alertObservers();
+    }
+
+    /**
+     * initializing the score
+     */
+    public void Score(String score)
+    {
+        this.Score = score;
     }
 }
